1df76c799fd0582076f22dc84c258206d25c7558
Manuel Ignacio Díaz Martínez <midiaz@sb.gob.do>
2021-02-26 20:43:52 +0100

    Add dtw similarity, probability of exceeding threshold, and out of sample dollar investment performance.

--- dtw.R
+++ dtw.R
@@ -19,7 +19,7 @@ library(collections)
 devAskNewPage(ask = FALSE)
 
 # Get S&P500 data
-start_date <- as.Date("2001-01-01")
+start_date <- as.Date("1970-01-01")
 spy <- Ad(getSymbols("SPY", auto.assign = FALSE, from=start_date, warning=FALSE))
 names(spy) <- "adjusted"
 spy_ts <- ts_ts(spy)
@@ -37,7 +37,7 @@ spy_ts <- na.remove(spy_ts)
 # - approach as regression problem - generate time series forecasts from the
 # original periods and combine them
 
-get_similar <- function(dl_ts=diff(log(spy_ts)), interval_length=30, skip_val=10, n_top_similar=10) {
+get_similar <- function(dl_ts=diff(log(spy_ts)), interval_length=45, skip_val=45, n_top_similar=20) {
   s_date_ref <- length(dl_ts) - interval_length + 1
   ref <- subset(dl_ts, start=s_date_ref)
   right_start_range <- s_date_ref - 2*interval_length
@@ -111,14 +111,69 @@ prob_exceeding <- function(similar_intervals, dl_ts=diff(log(spy_ts)), threshold
 
 ls <- get_similar()
 ints <- ls[[1]]
-prob_exceeding(ints, threshold=0.03)
+ref <- ls[[3]]
 
 
-numeric(5)
-
-top_similar_intervals
-min_distances
 
 
-spy_ts
-spy_ts %>% log() %>% diff() %>% autoplot()
+p <- autoplot(spy_ts)
+
+for (i in 1:length(ints)) {
+  interval <- ints[[i]]
+  start <- start(interval)
+  end <- end(interval)
+  window <- window(spy_ts, start=start, end=end)
+  p <- p + autolayer(window, colour = TRUE, series = paste(i))
+}
+
+p <- p + autolayer(window(spy_ts, start=start(ref), end=end(ref)), colour = TRUE, series="Current")
+
+print(p)
+
+top_int <- ints[[1]]
+top_int
+
+s <- start(top_int)
+e <- end(top_int)
+
+# dollar progress (during similar ints)
+for (interval in ints) {
+  curr <- numeric(length(interval) + 1)
+  curr[1] <- 1
+  for (i in 1:length(interval)) {
+    curr[i + 1] <- curr[i] * (1 + interval[i])
+  }
+  print(curr[length(curr)])
+}
+
+# dollar progress (out of sample)
+dollar_progress <- function(similar_intervals, dl_ts=diff(log(spy_ts)), threshold=0.05, window_length=22) {
+  dollar_performance <- matrix(nrow=(window_length+1), ncol=0)
+  for (i in 1:length(similar_intervals)) {
+    interval <- similar_intervals[[i]]
+    end_int <- end(interval)
+    freq <- frequency(interval)
+    start <- end_int+(1/freq)
+    end <- end_int + (window_length/freq)
+    window_after <- window(dl_ts, start=start, end = end)
+    
+    curr <- numeric(length(window_after) + 1)
+    curr[1] <- 1
+    for (i in 1:length(window_after)) {
+      curr[i + 1] <- curr[i] * (1 + window_after[i])
+    }
+    
+    dollar_performance <- cbind(dollar_performance, curr)
+  }
+  colnames(dollar_performance) <- 1:20
+  return(dollar_performance)
+}
+
+
+returns_mat <- dollar_progress(ints)
+matplot(returns_mat, type="l")
+
+
+mean(returns_mat[nrow(returns_mat), ])
+sd(returns_mat[nrow(returns_mat), ])
+# table with rank, start, end, 
